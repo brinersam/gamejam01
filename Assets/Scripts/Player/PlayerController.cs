7dadeb4fdfeeb25c1dd122cbaca4ca17c9567572
brinersam <vladbudyachenko@mail.ru>
2024-04-19 23:34:31 +0300

    Rigidbody movement (no more getting stuck)

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,15 +6,19 @@ namespace GJam.Player
 {
     public class PlayerController : MonoBehaviour
     {
-
-        private Vector3 _colliderSize = Vector3.one * 0.9f;
+    #region Collision
+        [SerializeField] private Rigidbody2D _rBody;
+        [SerializeField] private Collider2D _collider;
+        private const float MIN_COLLISION_DISTANCE = 0.1f;
+        private Vector3 _internalColliderSize = Vector3.one * 0.9f;
+    #endregion
 
     #region Movement
         [SerializeField] private float _moveForce = 2;
         private Vector2 _mvntVector;
         
         [SerializeField] private float _jumpForce = 1;
-        [Range(0,1)] [SerializeField] private float _jumpDecayTick = 0.1f;
+        [Range(0.3f,1)] [SerializeField] private float _jumpDecayTick = 0.1f;
         private Vector2 _jumpVector = Vector2.up;
         private float _jumpForceCur = 0;
 
@@ -22,52 +26,65 @@ namespace GJam.Player
         private Vector2 _gravityVector = Vector2.down;
     #endregion
 
-        private void Update()
+        private void FixedUpdate()
         {
-            ApplyForces();
+            ApplyMvmntForces();
         }
 
-        private void ApplyForces()
+        private void ApplyMvmntForces()
         {
-            if (_jumpForceCur > 0)
-                _jumpForceCur -= _jumpDecayTick;
-
             Vector3 movementDir = Vector3.zero;
+
+            _mvntVector = new Vector2(_mvntVector.x,0);
 
             movementDir += TryAddVector(_mvntVector, _moveForce);
             movementDir += TryAddVector(_gravityVector, _gravityForce);
             movementDir += TryAddVector(_jumpVector, _jumpForceCur);
 
-            transform.position += Time.deltaTime * movementDir;
+            transform.position += movementDir * Time.deltaTime;
+            Debug.Log(movementDir);
+
+            if (_jumpForceCur > 0)
+                _jumpForceCur -= _jumpDecayTick;
         }
 
         private Vector3 TryAddVector(Vector3 movementDirection, float force)
         {
-            if (MvmntBlocked(movementDirection,force) || force <= 0)
+            if (force <= 0)
                 return Vector3.zero;
 
-            // todo
-            // step (force * mvmntDir) is too big sometimes so we "walk" into collider and get stuck
-            // limit force here if far too deep into collider
+            // var collision = Collision(movementDirection,force);
 
-            return force * movementDirection;
+            // if (collision == true)
+            // {
+            //     if (collision.distance <= MIN_COLLISION_DISTANCE) // movement blocked
+            //         return Vector3.zero;
+                
+            //     if (collision.distance < .2f && force > collision.distance) // pillow to prevent getting stuck // activates at distance of .15
+            //         force = 0.1f;
+            // }
+
+            return (force - 0.1f) * movementDirection ;
         }
 
-        private bool MvmntBlocked(Vector3 movementDirection, float force)
+        private RaycastHit2D Collision(Vector3 movementDirection, float force)
         {
-            // if force is above "safe number" and clips then cast again using less force and if that succeeds, modify the force
             return Physics2D.BoxCast(
-                        transform.position,
-                        _colliderSize,
-                        0f,
-                        movementDirection,
-                        0.1f); //Math.Max(0.01f, force); 
+                    transform.position,
+                    _internalColliderSize,
+                    0f,
+                    movementDirection,
+                    force,
+                    layerMask: 0
+                    );
         }
 
         private void OnJump(InputValue input)
         {
-            if (_jumpForceCur > 0) // prevent jumping while midair from jumping (double jump before fall real??)
+            if (_jumpForceCur > 0) // prevent jumping while midair from jumping (double jump before fall real??) - use state to get isMidAir from
                 return;
+
+            //transform.position += Vector3.up * 0.1f; // in case of stuck jump out of it
 
             _jumpForceCur = _jumpForce + _gravityForce;
         }
